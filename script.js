b3a6cad7d870c826e531a68ad8839eef835d36ad
Utkarsh102002 <128974569+Utkarsh102002@users.noreply.github.com>
2025-10-02 15:17:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+let squares = document.querySelectorAll(".square");
+squares.forEach((square, index) => {
+  square.addEventListener("mouseenter", () => {
+    squares.forEach((sq) => {
+      if (sq === square) {
+        sq.style.backgroundColor = "#E6E6FA";
+      } else {
+        sq.style.backgroundColor = "#6F4E37";
+      }
+    });
+  });
+
+  square.addEventListener("mouseleave", () => {
+    squares.forEach((sq) => {
+      sq.style.backgroundColor = "#E6E6FA";
+    });
+  });
+});
